cb34ea1be629ddfe7e3aa1be087294b033d0bddb
dingyuc <dingyuc@yonyou.com>
2020-09-27 16:47:24 +0800

    2020-09-27 16:47

--- creatRedis/index.js
+++ creatRedis/index.js
@@ -1,5 +1,14 @@
 let redis = require('redis');
+let bluebird = require('bluebird');
 let config = require('../config/redis.js');
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
 let client = redis.createClient(config.port, config.host, {auth_pass: config.password});
+client.on('error', function (err) {
+    console.log('redis error: ', err);
+});
+client.on('connect', function(){
+    console.log('redis connect success!');
+});
 
 module.exports = client;
