6417144954a1b85fc91dd3f6060a0794517d830a
dingyuc <dingyuc@yonyou.com>
2020-10-02 22:11:55 +0800

    2020-10-02 22:11

--- asset/recreatePem.js
+++ asset/recreatePem.js
@@ -2,21 +2,22 @@ let NodeRSA = require('node-rsa');
 let fs = require('fs');
 let path = require('path');
 let isExist = fs.existsSync(path.resolve(__dirname, '../pem/public.pem'));
-if(!isExist) {
-    let key = new NodeRSA({ b: 512 });
-    key.setOptions({encryptionScheme: 'pkcs1'});
-    let privatePem = key.exportKey('pkcs1-private-pem');
-    let publicPem = key.exportKey('pkcs1-public-pem');
-    fs.writeFile(path.resolve(__dirname, '../pem/public.pem'), publicPem, (err) => {
-        if(err) {
-            throw err;
-        }
-        console.log('公钥已保存!');
-    });
-    fs.writeFile(path.resolve(__dirname, '../pem/private.pem'), privatePem, (err) => {
-        if(err) {
-            throw err;
-        }
-        console.log('私钥已保存!');
-    });
+if(isExist) {
+    return;
 }
+let key = new NodeRSA({ b: 512 });
+key.setOptions({encryptionScheme: 'pkcs1'});
+let privatePem = key.exportKey('pkcs1-private-pem');
+let publicPem = key.exportKey('pkcs1-public-pem');
+fs.writeFile(path.resolve(__dirname, '../pem/public.pem'), publicPem, (err) => {
+    if(err) {
+        throw err;
+    }
+    console.log('公钥已保存!');
+});
+fs.writeFile(path.resolve(__dirname, '../pem/private.pem'), privatePem, (err) => {
+    if(err) {
+        throw err;
+    }
+    console.log('私钥已保存!');
+});
